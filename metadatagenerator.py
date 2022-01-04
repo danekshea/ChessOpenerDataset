474ec130e5bb6efd39bf83f631af9b5eb147c2fb
danekshea <danekshea@gmail.com>
2022-01-04 14:23:01 +1100

    Added parser for additional dataset

--- metadatagenerator.py
+++ metadatagenerator.py
@@ -4,62 +4,108 @@ import chess.svg
 from contextlib import redirect_stdout
 import subprocess
 
-#Open file of openers
-with open('P3eco.txt') as f:
-    lines = f.readlines()
+#function for generating metadata from P3eco.txt opener dataset
+def p3ecotextgenerator():
+    tokenid = 7
+    tokens = {}
 
-tokenid = 7
-tokens = {}
-
-#Prefixes for ECO
-prefixes = ['A', 'B', 'C', 'D', 'E']
-
-#Split the openers, remove the comments, merge multiple line PGNs and JSONify everything
-for i, line in enumerate(lines):
-    if not line.startswith(';'):
-        #identify eco lines
-        if line.startswith(tuple(prefixes)):
-            econame = line.split(' ', 1)
-            #split comments from names and cleanup
-            if ';' in econame[1]:
-                namecomment = econame[1].split(';')
-                econame[1] = namecomment[0]
-            econame[1] = econame[1].replace('\r\n', '')
-            econame[1] = econame[1].rstrip()
-            econame[1] = econame[1].lstrip()
-            pgn=lines[i+1]
-            pgn=pgn.strip()
-        
-            #Accounts for multiple line pgns from the source file
-            if not pgn.endswith('1/2'):
-                pgn = pgn + ' ' + lines[i+2]
+    #Prefixes for ECO
+    prefixes = ['A', 'B', 'C', 'D', 'E']
+    
+    #Open file of openers
+    with open('raw data/P3eco.txt') as f:
+        lines = f.readlines()
+    
+    #Split the openers, remove the comments, merge multiple line PGNs and JSONify everything
+    for i, line in enumerate(lines):
+        if not line.startswith(';'):
+            #identify eco lines
+            if line.startswith(tuple(prefixes)):
+                econame = line.split(' ', 1)
+                #split comments from names and cleanup
+                if ';' in econame[1]:
+                    namecomment = econame[1].split(';')
+                    econame[1] = namecomment[0]
+                econame[1] = econame[1].replace('\r\n', '')
+                econame[1] = econame[1].rstrip()
+                econame[1] = econame[1].lstrip()
+                pgn=lines[i+1]
+                pgn=pgn.strip()
+            
+                #Accounts for multiple line pgns from the source file
                 if not pgn.endswith('1/2'):
-                    pgn = pgn + ' ' + lines[i+3]
+                    pgn = pgn + ' ' + lines[i+2]
+                    if not pgn.endswith('1/2'):
+                        pgn = pgn + ' ' + lines[i+3]
 
-            #clean up the PGN and write to file for use with pgn-extract     
-            pgn=pgn.replace('\r\n', '').replace('(', '').replace(')', '').strip()
-            with open('pgn.txt', 'w') as outfile:
-                outfile.write(pgn)
+                #clean up the PGN and write to file for use with pgn-extract     
+                pgn=pgn.replace('\r\n', '').replace('(', '').replace(')', '').strip()
+                with open('pgn.txt', 'w') as outfile:
+                    outfile.write(pgn)
 
-            #create the FEN string using pgn-extract
-            FEN = str(subprocess.check_output("/Users/daneshea/Projects/ChessOpenerDataset/pgn-extract --nomovenumbers --noresults --notags -C -F pgn.txt | grep '{.*}'", shell=True))
-            FEN = FEN.split("\"")
-            FEN = FEN[1]
+                #create the FEN string using pgn-extract
+                FEN = str(subprocess.check_output("/Users/daneshea/Projects/ChessOpenerDataset/pgn-extract --nomovenumbers --noresults --notags -C -F pgn.txt | grep '{.*}'", shell=True))
+                FEN = FEN.split("\"")
+                FEN = FEN[1]
 
-            #create token metadata and write to individual files
-            tokens[tokenid] = {'tokenid': tokenid, 'eco': econame[0], 'name': econame[1], 'pgn': pgn, 'FEN': FEN, 'image_url': 'https://raw.githubusercontent.com/danekshea/ChessOpenerDataset/master/metadata/images/' + str(tokenid) + '.svg'}
+                #create SVGs of board states
+                board = chess.Board(FEN)
+                with open('metadata/images/' + str(tokenid) + '.svg', 'w') as f:
+                    with redirect_stdout(f):
+                            print(chess.svg.board(board, size=350))
 
-            #write metadata JSON files
-            with open('metadata/' + str(tokenid), 'w') as outfile:
-                data =json.dump(tokens[tokenid], outfile, indent=4)
+                #create token metadata and write to individual files
+                tokens[tokenid] = {'tokenid': tokenid, 'eco': econame[0], 'name': econame[1], 'pgn': pgn, 'FEN': FEN, 'image_url': 'https://raw.githubusercontent.com/danekshea/ChessOpenerDataset/master/metadata/images/' + str(tokenid) + '.svg'}
 
-            #create SVGs of board states
-            board = chess.Board(FEN)
-            with open('metadata/images/' + str(tokenid) + '.svg', 'w') as f:
-                with redirect_stdout(f):
-                        print(chess.svg.board(board, size=350))
+                #write metadata JSON files
+                with open('metadata/' + str(tokenid), 'w') as outfile:
+                    data =json.dump(tokens[tokenid], outfile, indent=4)
 
-            tokenid += 1
+                tokenid += 1
+
+#function for generating metadata from raw data/niklasfchessopenings.txt
+def niklasfchessopeninggenerator():
+    tokenid = 7
+    tokens = {}
+    
+    #Open file of openers
+    with open('raw data/niklasfchessopenings.txt') as f:
+        lines = f.readlines()
+    
+    #Split the openers, remove the comments, merge multiple line PGNs and JSONify everything
+    for i, line in enumerate(lines):
+        eco = line[0:3]
+        
+        namepgn = line[3:].split('1.')
+
+        name = namepgn[0].strip('\t')
+        pgn = namepgn[1].strip('\n')
+        pgn = '1.' + pgn + ' 1/2'
+
+        with open('pgn.txt', 'w') as outfile:
+                    outfile.write(pgn)
+
+        #create the FEN string using pgn-extract
+        FEN = str(subprocess.check_output("/Users/daneshea/Projects/ChessOpenerDataset/pgn-extract --nomovenumbers --noresults --notags -C -F pgn.txt | grep '{.*}'", shell=True))
+        FEN = FEN.split("\"")
+        FEN = FEN[1]
+
+        #create SVGs of board states
+        board = chess.Board(FEN)
+        with open('metadata/images/' + str(tokenid) + '.svg', 'w') as f:
+            with redirect_stdout(f):
+                    print(chess.svg.board(board, size=350))
+
+        #create token metadata and write to individual files
+        tokens[tokenid] = {'tokenid': tokenid, 'eco': eco, 'name': name, 'pgn': pgn, 'FEN': FEN, 'image_url': 'https://raw.githubusercontent.com/danekshea/ChessOpenerDataset/master/metadata/images/' + str(tokenid) + '.svg'}
+
+        #write metadata JSON files
+        with open('metadata/' + str(tokenid), 'w') as outfile:
+           data =json.dump(tokens[tokenid], outfile, indent=4)
+
+        tokenid += 1    
+
+niklasfchessopeninggenerator()
 
 #Write the entire token dictionary to a file in JSON
 #with open('test.txt', 'w') as outfile:
