a91108c4ee4f0d1e6cedacac705abfa9662961db
danekshea <danekshea@gmail.com>
2022-11-09 21:48:14 +1100

    deleted ipfs posting

--- ipfsposting.py
+++
@@ -1,7 +0,0 @@
-import requests, urllib
-
-NODE = "http://localhost:5001"
-FILE_PATH = "./metadata/8" # path to file you're trying to add
-MFS_PATH = "/metadata"   # mfs path you're trying to write to
-
-response = requests.post(NODE+"/api/v0/files/write?arg=%s&create=true" % urllib.parse.quote(MFS_PATH), files={FILE_PATH:open(FILE_PATH, 'rb')})
